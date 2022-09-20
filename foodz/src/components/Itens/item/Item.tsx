f7bc31e6dac399cab93e1cebb40b277a5614f116
luizhgrodrigues <luizhgrodrigues@outlook.com>
2022-09-20 11:06:40 -0300

    estilização e responsividade

--- foodz/src/components/Itens/item/Item.tsx
+++ foodz/src/components/Itens/item/Item.tsx
@@ -2,6 +2,9 @@
 import styles from "./Item.module.scss";
 import classNames from 'classnames'
 
+import { MdPeople } from "react-icons/md";
+
+
 interface Props {
   title: string;
   description: string;
@@ -32,8 +35,8 @@ export default function Item(props: Props) {
             [styles.item__tipo]: true,
             [styles[`item__tipo__${props.category.label.toLowerCase()}`]]: true
           })}>{props.category.label}</div>
-          <div className={styles.item__porcao}>{props.size}</div>
-          <div className={styles.item__qtdpessoas}>{props.serving}</div>
+          <div className={styles.item__porcao}>  {props.size}g</div>
+          <div className={styles.item__qtdpessoas}> <MdPeople size={25} className={styles.item__icon}/> {props.serving}</div>
           <div className={styles.item__valor}>R$ { props.price },00</div>
         </div>
       </div>
